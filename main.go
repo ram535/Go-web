3eed8ae3cfec0a72c28f9406e6c3a5fb1cd4285c
last <ramses.aldama@gmail.com>
2019-06-20 08:44:19 +0200

    add a 404 page using gorilla mux

--- main.go
+++ main.go
@@ -17,8 +17,33 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "To get in touch, please send a email to <a href=\"mailto:support@foo.com\">suppurt@foo.com</a>")
 }
 
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>Page not found</h1>")
+}
 func main() {
 	r := mux.NewRouter()
+	// this is use when no route match
+	// it is a build in variable of type http.Handler
+	// it use by default a predifine 404 page
+	// we change it to use a custom 404 page
+	//
+	// type HandlerFunc func(ResponseWriter, *Request)
+	// func (f HandlerFunc) ServerHTTP(w ResponseWriter, r *Rquest)
+	// type Handler interface {
+	//    ServerHTTP(ResponseWriter, *Request)
+	// }
+	//
+	// NotFoundHandler has to be of type http.Handler
+	// that's why we transform it with http.HandlerFunc()
+	// I understand we can transform it because the underline
+	// type of HandlerFunc is the same as the signature of notFound
+	// so by transforming notFound to http.HandlerFunc, it also become
+	// of type http.Handler because http.HandlerFunc has the ServerHTTP()
+	// to be able to also be of the type http.Handler because it match
+	// the interface of http.Handler
+	r.NotFoundHandler = http.HandlerFunc(notFound)
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	http.ListenAndServe(":3000", r)
