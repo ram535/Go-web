7575495ffd207bfb66f86c8a578eeb3873dfdc1f
last <ramses.aldama@gmail.com>
2019-06-24 00:48:52 +0200

    Creating the home view

--- main.go
+++ main.go
@@ -1,34 +1,47 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
-	"os"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-type Dog struct {
-	Name string
+// we could have put this variable inside the func home
+// but that would create a home page everytime the
+// func home is called
+var homeTemplate *template.Template
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if err := homeTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
-type User struct {
-	Dog   Dog
-	Slice []string
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, please send a email to <a href=\"mailto:support@foo.com\">suppurt@foo.com</a>")
 }
 
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>Page not found</h1>")
+}
 func main() {
-	t, err := template.ParseFiles("hello.gohtml")
+	// we are declering err like that so we can use the global variable
+	// homTemplate, otherwise if we use a :=, we would be creating a local
+	// homeTemplate variable
+	var err error
+	homeTemplate, err = template.ParseFiles("views/home.gohtml")
 	if err != nil {
 		panic(err)
 	}
-
-	data := User{
-		Dog: Dog{
-			Name: "foo bar",
-		},
-		Slice: []string{"a", "b", "c"},
-	}
-
-	err = t.Execute(os.Stdout, data)
-	if err != nil {
-		panic(err)
-	}
+	r := mux.NewRouter()
+	r.NotFoundHandler = http.HandlerFunc(notFound)
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":3000", r)
 }
