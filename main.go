90b38540aeb80a758f0b20615ebe4083a31a4869
last <ramses.aldama@gmail.com>
2019-07-16 02:36:14 +0200

    Create a controller for our static pages, and we also update the View type to implement the http.Handler interface

--- main.go
+++ main.go
@@ -3,27 +3,10 @@ package main
 import (
 	"fmt"
 	"myGo/Go-web/controllers"
-	"myGo/Go-web/views"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
-
-// we could have put this variable inside the func home
-// but that would create a home page everytime the
-// func home is called
-var homeView *views.View
-var contactView *views.View
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(homeView.Render(w, nil))
-}
-
-func contact(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(contactView.Render(w, nil))
-}
 
 func notFound(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -31,21 +14,15 @@ func notFound(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Page not found</h1>")
 }
 func main() {
-	// we are passing the home.gohtml file to the views.NewView() method
-	// So this method will parse this file with the other files (booktabs.gohtml, footer.gohtml, navbar.gohtml)
-	// So the program is going to be aware which files exist
-	// So like noew we can call the "bootsrap template"
-	// above in the homeView.Template.Executetemplate() method
-	// and this one can call the "home template, navbar template and footer template"
-	homeView = views.NewView("bootstrap", "views/home.gohtml")
-	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	staticC := controllers.NewStatic()
 	userC := controllers.NewUsers()
 
 	r := mux.NewRouter()
 	r.NotFoundHandler = http.HandlerFunc(notFound)
-	r.HandleFunc("/", home).Methods("GET")
-	r.HandleFunc("/contact", contact).Methods("GET")
-	r.HandleFunc("/signup", userC.New).Methods("GET")
+	r.Handle("/", staticC.Home).Methods("GET")
+	r.Handle("/contact", staticC.Contact).Methods("GET")
+	r.Handle("/signup", userC.NewView).Methods("GET")
+	// r.HandleFunc("/signup", userC.New).Methods("GET")
 	r.HandleFunc("/signup", userC.Create).Methods("POST")
 	http.ListenAndServe(":3000", r)
 }
